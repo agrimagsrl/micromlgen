874923d467c29e04ea7268c8840dd3a3d1f4d2db
Agrimagsrl <web@agrimag.it>
2019-11-28 19:57:14 +0100

    first commit

---
+++ micromlgen_test.py
@@ -0,0 +1,11 @@
+from micromlgen import port
+from sklearn.svm import SVC
+from sklearn.datasets import load_iris
+
+
+if __name__ == '__main__':
+    iris = load_iris()
+    X = iris.data
+    y = iris.target
+    clf = SVC(gamma=1).fit(X, y)
+    print(port(clf))
